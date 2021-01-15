f6aada50f7ff7a9a8ac6a427c91e21d8f57be9ad
Bohdan Matushevskyi <bohdan.chorna@gmail.com>
2021-01-15 20:33:12 +0200

    fixed "fast food" homework

--- HW/hw_16Classes/main.js
+++ HW/hw_16Classes/main.js
@@ -47,76 +47,60 @@ console.log("Price with sauce: “ + hamburger.calculatePrice());
  */
 
 class Hamburger {
-    price = 0;
-    calories = 0;
-
-    sizeSmall() {
-        this.price += 50;
-        this.calories += 20;
-        return this.price, this.calories;
+    sizeSmall = {//усі опції записав об'єктами
+        price: 50,
+        calories: 20,
     }
-    sizeBbig() {
-        this.price += 100;
-        this.calories += 40;
-        return this.price, this.calories;
+    sizeBig = {
+        price: 100,
+        calories: 40,
     }
-    stuffingCheze() {
-        this.price += 10;
-        this.calories += 20;
-        return this.price, this.calories;
+    stuffingCheesev = {
+        price: 10,
+        calories: 20,
     }
-    stuffingSalad() {
-        this.price += 20;
-        this.calories += 5;
-        return this.price, this.calories;
+    stuffingSalad = {
+        price: 20,
+        calories: 5,
     }
-    stuffingPotato() {
-        this.price += 15;
-        this.calories += 10;
-        return this.price, this.calories;
+    stuffingPotato = {
+        price: 15,
+        calories: 10,
     }
-    toppingSause() {
-        this.price += 15;
-        this.calories += 0;
-        return this.price, this.calories;
+    toppingSause = {
+        price: 15,
+        calories: 0,
     }
-    toppingMayo() {
-        this.price += 20;
-        this.calories += 5;
-        return this.price, this.calories;
+    toppingMayo = {
+        price: 20,
+        calories: 5,
+    }
+    clientHamburger = [];//масив для обраних опцій
+    add(item){//метод для додавання обраних опцій до масиву
+        this.clientHamburger.push(item)
+        return this.clientHamburger
     }
     calculatePrice() {
-        return this.price + " тугриков";
+        let price = 0;
+        this.clientHamburger.forEach(function(elem) {//сумую ціни кожної обраної опції
+            price += Number(elem.price);
+        })
+        return price + " тугриків";
     }
     calculateCalories() {
-        return this.calories + " калорий"
+        let calories = 0; 
+        this.clientHamburger.forEach(function(elem) {//сумую калорії кожної обраної опції
+            calories += Number(elem.calories);
+        })
+        return calories + " калорій";
     }
 }
 
-let sandwich = new Hamburger;
-
-console.log(`Сеть фастфудов предлагает несколько видов гамбургеров:
-
-    маленький (50 тугриков, 20 калорий)
-    большой (100 тугриков, 40 калорий)
-
-Гамбургер может быть с одним из нескольких видов начинок:
-
-    сыром (+ 10 тугриков, + 20 калорий)
-    салатом (+ 20 тугриков, + 5 калорий)
-    картофелем (+ 15 тугриков, + 10 калорий)
-
-Можно добавить добавки:
-
-    посыпать приправой (+ 15 тугриков, 0 калорий)
-    полить майонезом (+ 20 тугриков, + 5 калорий).
-`);
-
-sandwich.sizeSmall();//малий розмір
-sandwich.stuffingCheze();//начинка сир
-sandwich.toppingMayo();//добавка майонез
-console.log("Ваш заказ");
-console.log("Маленький сэндвич с сыром и майонезом: " + sandwich.calculateCalories());//інформація про калорії
-console.log("Цена маленького сэндвича с сыром и майонезом: " + sandwich.calculatePrice());//інформація про ціну
-sandwich.toppingSause();//добавка соус
-console.log("Цена с соусом: " + sandwich.calculatePrice());//інформація про ціну
+let hamburger = new Hamburger;//використовую клас, створюю об'єкт 
+hamburger.add(hamburger.sizeBig);//додаю різні опції
+hamburger.add(hamburger.stuffingCheesev);
+hamburger.add(hamburger.stuffingSalad);
+hamburger.add(hamburger.toppingMayo);
+console.table(hamburger.clientHamburger);//виводжу клієнтське замовлення
+console.log(hamburger.calculatePrice());//підраховую ціну
+console.log(hamburger.calculateCalories());//підраховую калорії
